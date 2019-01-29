3d3adcae597b12d3d916d0ec3786ed355efb4ea4
Paul Costa <paulcosta.student@careerdevs.com>
2019-01-29 01:53:43 +0000

    updated

---
+++ function3.js
@@ -0,0 +1,22 @@
+function makeTurkeySandwich() {
+    get one slice of bread;
+    add turkey;
+    put a slice of bread on top;
+}
+
+makeTurkeySandwich()
+
+makeSandwichWith _____
+    get one slice of bread;
+    add ____;
+    put a slice of bread on top;
+
+function makeSandwichWith(filling) {
+    get one slice of bread;
+    add filling;
+    put a slice of bread on top;
+}
+
+makeTurkeySandwich(ham)    
+makeTurkeySandwich("ham")  text only
+console.log("My Sandwich:", ham)
